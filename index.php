9d3b888f53a2b9baf3532549f1aaec7e082f1fd0
Harry Wood <harry@harrywood.co.uk>
2016-01-10 21:27:16 +0100

    add current holding page HTML

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>OpenStreetMap GB</title>
+</head>
+<body>
+<div align="center" style="margin-top:60px;">
+<img src="osmgb-logo.png">
+<br><br>
+<h3>OSMGB is sleeping. Back soon hopefully</h3>
+<br><br><a href="http://www.nottingham.ac.uk/ngi/research/geospatial-science/projects/osm-gbnew.aspx">Nottingham University website has more information</a> on this project
+<br><br>
+Looking for the OpenStreetMap '<a href="http://wiki.openstreetmap.org/wiki/GB/UK_Chapter">GB Chapter</a>'? It doesn't exist (yet)
+</div>
+</body>
